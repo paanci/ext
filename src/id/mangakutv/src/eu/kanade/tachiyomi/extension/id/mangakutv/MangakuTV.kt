21d5783026cada15bf79fb29580b7e30f1804d40
paanci <paanci007@gmail.com>
2024-12-02 23:13:52 +0700

    Update MangakuTV.kt

--- src/id/mangakutv/src/eu/kanade/tachiyomi/extension/id/mangakutv/MangakuTV.kt
+++ src/id/mangakutv/src/eu/kanade/tachiyomi/extension/id/mangakutv/MangakuTV.kt
@@ -1,10 +1,10 @@
 package eu.kanade.tachiyomi.extension.en.mangakutv
 
+import android.util.Base64
+import eu.kanade.tachiyomi.source.model.Page
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
-import android.util.Base64
-import eu.kanade.tachiyomi.source.model.Page
 import java.text.SimpleDateFormat
 import java.util.Locale
 
@@ -15,36 +15,36 @@ class MangakuTV : Madara(
     dateFormat = SimpleDateFormat("MMM d, yyyy", Locale.US),
 ) {
 
-    override val mangaSubString = "manga" 
+    override val mangaSubString = "manga"
 
     override fun pageListParse(document: Document): List<Page> {
-    return document.select("div.reading-content img.wp-manga-chapter-img").mapIndexed { i, img ->
-        // Ambil atribut data
-        val encodedData = img.attr("data")
+        return document.select("div.reading-content img.wp-manga-chapter-img").mapIndexed { i, img ->
+            // Ambil atribut data
+            val encodedData = img.attr("data")
 
-        // Proses decoding
-        val decodedUrl = decodeImageUrl(encodedData)
+            // Proses decoding
+            val decodedUrl = decodeImageUrl(encodedData)
 
-        // Return Page dejngan URL yang sudah di-decode
-        Page(i, "", decodedUrl)
-    }
-}
-
-// Fungsi untuk decode URL dari data
-fun decodeImageUrl(encodedData: String): String {
-    // Decode pertama dengan Base64
-    val base64Decoded = String(android.util.Base64.decode(encodedData, android.util.Base64.DEFAULT))
-
-    // Decode ROT13
-    val rot13Decoded = base64Decoded.map {
-        when (it) {
-            in 'A'..'Z' -> 'A' + (it - 'A' + 13) % 26
-            in 'a'..'z' -> 'a' + (it - 'a' + 13) % 26
-            else -> it
+            // Return Page dengan URL yang sudah di-decode
+            Page(i, "", decodedUrl)
         }
-    }.joinToString("")
+    }
 
-    // Decode kedua dengan Base64
-    return String(android.util.Base64.decode(rot13Decoded, android.util.Base64.DEFAULT))
-}
+    // Fungsi untuk decode URL dari data
+    fun decodeImageUrl(encodedData: String): String {
+        // Decode pertama dengan Base64
+        val base64Decoded = String(android.util.Base64.decode(encodedData, android.util.Base64.DEFAULT))
+
+        // Decode ROT13
+        val rot13Decoded = base64Decoded.map {
+            when (it) {
+                in 'A'..'Z' -> 'A' + (it - 'A' + 13) % 26
+                in 'a'..'z' -> 'a' + (it - 'a' + 13) % 26
+                else -> it
+            }
+        }.joinToString("")
+
+        // Decode kedua dengan Base64
+        return String(android.util.Base64.decode(rot13Decoded, android.util.Base64.DEFAULT))
+    }
 }
